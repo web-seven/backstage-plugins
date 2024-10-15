38d013c211f1bcd466a1f5544f98887bdf1f6013
Catalin Jumiga <catalinjumiga4@gmail.com>
2024-10-15 18:21:14 +0300

    fixed conflicts for commit:implemented required changes relating logging and grammatical issues

--- plugins/backstage-plugin-openfga-backend/src/permissions/permissionsCollector.ts
+++ plugins/backstage-plugin-openfga-backend/src/permissions/permissionsCollector.ts
@@ -1,8 +1,9 @@
 import { Config } from '@backstage/config';
 import axios from 'axios';
+import { LoggerService  } from '@backstage/backend-plugin-api';
 
-export async function collectPermissions(config: Config): Promise<{ [name: string]: string[] }> {
-  const urls = permitionsPaths(config);
+export async function collectPermissions(config: Config, logger: LoggerService): Promise<{ [name: string]: string[] }> {
+  const urls = permissionsPaths(config);
   const permissions: Array<any> = [];
 
   for (const url of urls) {
@@ -11,26 +12,26 @@ export async function collectPermissions(config: Config): Promise<{ [name: strin
       if (result.data.permissions) {
         permissions.push(...result.data.permissions);
       } else {
-        console.log('No permissions found at:', url);
+        logger.error('No permissions found');
       }
     } catch (error) {
       if (axios.isAxiosError(error) && error.response?.status === 404) {
-        console.log(`URL not found: ${url}`);
+        logger.error(`URL not found: ${url}`);
       } else {
-        console.error(`Error fetching permissions from ${url}:`, error);
+        logger.error(`Error fetching permissions from ${url}`);
       }
     }
   }
 
   if (permissions.length > 0) {
-    const permissionData = changeArrayStructure(permissions);
+    const permissionData = changeArrayStructure(permissions, logger);
     return permissionData;
   } else {
     return {};
   }
 }
 
-function changeArrayStructure<T extends { name: string }>(permissions: Array<T>): { [name: string]: string[] } {
+function changeArrayStructure<T extends { name: string}>(permissions: Array<T>, logger: LoggerService): { [name: string]: string[] } {
   const permissionMap: { [name: string]: string[] } = {};
 
   permissions.forEach((permission: any) => {
@@ -45,17 +46,17 @@ function changeArrayStructure<T extends { name: string }>(permissions: Array<T>)
         }
         permissionMap[name].push(action);
       } else {
-        console.warn('Permission action is undefined for:', permission);
-      }
-    } else {
-      console.warn('Permission name is undefined:', permission);
+        logger.error('Permission action is undefined for:', permission);
+            }
+          } else {
+            logger.error('Permission name is undefined:', permission);
     }
   });
 
   return permissionMap;
 }
 
-function permitionsPaths(config: Config) {
+function permissionsPaths(config: Config) {
   const baseurl = config.getOptionalString('backend.baseUrl') ?? '';
   const pluginsList = config.getOptionalStringArray('openfga.plugins') ?? [];
   let paths = [];
--- plugins/backstage-plugin-openfga-backend/src/service/router.ts
+++ plugins/backstage-plugin-openfga-backend/src/service/router.ts
@@ -1,11 +1,11 @@
 import { MiddlewareFactory } from '@backstage/backend-defaults/rootHttpRouter';
-import express from 'express';
-import Router from 'express-promise-router';
 import { OpenfgaRoutesService } from '../OpenfgaRoutesService';
 import { RouterOptions } from '../types';
 import { LoggerService  } from '@backstage/backend-plugin-api';
 import { Config } from '@backstage/config';
-import {collectPermissions} from '../permitions/permissionsCollector'
+import express from 'express';
+import Router from 'express-promise-router'
+import {collectPermissions} from '../permissions/permissionsCollector'
 import {createAuthorizationModel} from '../fga/openfgaClient'
 
 export async function createRouter(
@@ -20,14 +20,14 @@ export async function createRouter(
     logger.info('PONG!');
   response.json({ status: 'ok' });
   });
-
-  router.get('/extract-permitions', async (_, response) => {
-    const permissions = await collectPermissions(config);
+ 
+  router.get('/extract-permissions', async (_, response) => {
+    const permissions = await collectPermissions(config, logger);
     response.json({ data: permissions });
   });
 
   router.get('/create-authorization-model', async (_, response) => {
-    const permitionList = await collectPermissions(config);
+    const permitionList = await collectPermissions(config, logger);
     const model = await createAuthorizationModel(permitionList, config);
 
     response.json({ data: model });
