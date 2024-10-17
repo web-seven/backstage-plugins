88682fee99da5efc6bf6f46e1211b845bc0d423c
Evgheni Poleacov <evgheni.poleacov@gmail.com>
2024-10-17 09:45:04 +0300

    prototype of get clusters details

--- plugins/backstage-module-crossplane/src/module.ts
+++ plugins/backstage-module-crossplane/src/module.ts
@@ -3,19 +3,61 @@ import {
   createBackendModule,
 } from '@backstage/backend-plugin-api';
 
-import { catalogProcessingExtensionPoint } from '@backstage/plugin-catalog-node';
+import { KubernetesBuilder } from '@backstage/plugin-kubernetes-backend';
+import { catalogServiceRef } from '@backstage/plugin-catalog-node/alpha';
+
 
 export const catalogModuleCrossplane = createBackendModule({
   pluginId: 'kubernetes',
   moduleId: 'crossplane',
   register(reg) {
     reg.registerInit({
-      deps: { 
-        logger: coreServices.logger, 
-        fetcher: catalogProcessingExtensionPoint 
+      deps: {
+        http: coreServices.httpRouter,
+        logger: coreServices.logger,
+        config: coreServices.rootConfig,
+        discovery: coreServices.discovery,
+        catalogApi: catalogServiceRef,
+        permissions: coreServices.permissions,
+        auth: coreServices.auth,
+        httpAuth: coreServices.httpAuth,
       },
-      async init({ logger }) {
-        logger.info('Hello World!');
+      async init({
+        logger, 
+        config, 
+        catalogApi, 
+        permissions, 
+        discovery, 
+        httpAuth, 
+        auth, 
+      }) {
+        const builder: KubernetesBuilder = KubernetesBuilder.createBuilder({
+          logger,
+          config,
+          catalogApi,
+          permissions,
+          discovery,
+          auth,
+          httpAuth,
+        })
+
+        const { fetcher, clusterSupplier } = await builder.build();
+        let clusters = await clusterSupplier.getClusters({
+          credentials: await auth.getNoneCredentials()
+        })
+        console.log(clusters)
+        // fetcher.fetchObjectsForService({
+        //   serviceId: 'some-service',
+        //   clusterDetails: {
+        //     name: 'cluster1',
+        //     url: 'http://localhost:9999',
+        //     authMetadata: {},
+        //   },
+        //   credential: { type: 'bearer token', token: 'token' },
+        //   objectTypesToFetch: OBJECTS_TO_FETCH,
+        //   labelSelector: '',
+        //   customResources: [],
+        // })
       },
     });
   },
