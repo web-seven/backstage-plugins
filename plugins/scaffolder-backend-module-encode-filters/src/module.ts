ae3b63df4a800d3079466df7e267034b67674f34
ionition <vlad.ionita25.04@gmail.com>
2024-12-26 17:59:12 +0200

    removed compression of formState

--- plugins/scaffolder-backend-module-encode-filters/src/module.ts
+++ plugins/scaffolder-backend-module-encode-filters/src/module.ts
@@ -1,4 +1,3 @@
-import Pako from 'pako';
 import { JsonValue } from '@backstage/types';
 import { scaffolderTemplatingExtensionPoint } from '@backstage/plugin-scaffolder-node/alpha';
 import { createBackendModule } from '@backstage/backend-plugin-api';
@@ -14,12 +13,6 @@ export const encodeFilters = createBackendModule({
       async init({ scaffolder }) {
         scaffolder.addTemplateFilters({
           base64: (...args: JsonValue[]) => btoa(args.join('')),
-          compress: (...args: JsonValue[]) => {
-            const compressed = Pako.gzip(args.join(''));
-            const compressedBase64 = Buffer.from(compressed).toString('base64');
-
-            return compressedBase64;
-          },
         });
       },
     });
