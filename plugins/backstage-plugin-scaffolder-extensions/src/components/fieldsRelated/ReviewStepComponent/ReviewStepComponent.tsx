24e675cb8bb75ca9bee1373e6adeb18e9cc0a378
ionition <vlad.ionita25.04@gmail.com>
2025-01-15 17:04:50 +0200

    using custom buttons presentations on review step

--- plugins/backstage-plugin-scaffolder-extensions/src/components/fieldsRelated/ReviewStepComponent/ReviewStepComponent.tsx
+++ plugins/backstage-plugin-scaffolder-extensions/src/components/fieldsRelated/ReviewStepComponent/ReviewStepComponent.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import {
   ReviewState,
   ParsedTemplateSchema,
+  useTemplateParameterSchema,
+  useFilteredSchemaProperties,
+  useTemplateSchema,
 } from '@backstage/plugin-scaffolder-react/alpha';
 import { ReviewStepProps } from '@backstage/plugin-scaffolder-react';
 import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { mergeSchemas, replaceEntityObjectWithLink } from '../../../utils';
+import { useRouteRefParams } from '@backstage/core-plugin-api';
+import { scaffolderPlugin } from '@backstage/plugin-scaffolder';
+import { stringifyEntityRef } from '@backstage/catalog-model';
+
 /**
  * Custom review step component
  *
@@ -30,8 +37,33 @@ export const ReviewStepComponent = (props: ReviewStepProps): JSX.Element => {
   const styles = useStyles();
   const { formData, handleBack, steps, handleCreate } = props;
 
-  if (formData.formState) {
-    delete formData.formState;
+  if (formData.formState) delete formData.formState;
+
+  const { templateName, namespace } = useRouteRefParams(
+    scaffolderPlugin.routes.selectedTemplate,
+  );
+
+  const templateRef = stringifyEntityRef({
+    kind: 'Template',
+    namespace: namespace,
+    name: templateName,
+  });
+
+  const { manifest } = useTemplateParameterSchema(templateRef);
+  const sortedManifest = useFilteredSchemaProperties(manifest);
+
+  let backLabel = 'Back';
+  let createLabel = 'Create';
+
+  const { presentation } = useTemplateSchema(
+    sortedManifest
+      ? sortedManifest
+      : { title: '', steps: [{ title: '', schema: {} }] },
+  );
+
+  if (presentation?.buttonLabels) {
+    backLabel = presentation.buttonLabels.backButtonText ?? backLabel;
+    createLabel = presentation.buttonLabels.createButtonText ?? createLabel;
   }
 
   return (
@@ -45,10 +77,10 @@ export const ReviewStepComponent = (props: ReviewStepProps): JSX.Element => {
       />
       <div className={styles.footer}>
         <Button onClick={handleBack} className={styles.backButton}>
-          Back
+          {backLabel}
         </Button>
         <Button variant="contained" color="primary" onClick={handleCreate}>
-          Create
+          {createLabel}
         </Button>
       </div>
     </>
